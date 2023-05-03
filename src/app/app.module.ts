ce3d8cef4ede0f3cece4ac13ab689b34c4578385
DavoSagha <saghatelyandsm@gmail.com>
2023-05-02 23:14:13 -0300

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
   declarations: [
@@ -26,12 +29,15 @@ import { FooterComponent } from './components/footer/footer.component';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/logo-ap/logo-ap.component.ts
+++ src/app/components/logo-ap/logo-ap.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo-ap',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class LogoAPComponent {
 
+  constructor(private router:Router){
+
+  }
+
+
+  login(){
+    this.router.navigate(['/login']);
+  }
 }
